9a61b148d2ab0a26af95b1321607ba9662e6f9c6
chi <nguyenlinhchi09@gmail.com>
2019-04-22 00:26:47 +0200

    2 content

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   get 'static_pages/leher'
   root 'static_pages#leher'
+  get '/preference', to: 'static_pages#preference'
+  get '/zerosum', to: 'static_pages#zerosum'
 end
